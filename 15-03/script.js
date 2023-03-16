778a9df0c7a51edeade5d8fe90990fa2632346bb
Nurgul <abduhamidovanurgul@gmail.com>
2023-03-16 11:33:30 +0600

    start tasks

---
+++ 15-03/script.js
@@ -0,0 +1,14 @@
+let name = 'Nurgul';
+console.log(`Привет ${name}`);
+
+
+
+let age = 21;
+console.log(`Мне ${age} лет!`);
+
+
+let strl1 = 'Салам,';
+let strl2 = 'Бишкек!';
+console.log(`${strl1} ${strl2}`);
+
+
---
+++ 16-03/script.js
@@ -0,0 +1,21 @@
+// 1
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+console.log(result);
+
+// 2
+let hour = 2;
+let min = 10;
+let seconds = 30;
+console.log(`${hour} : ${min} : ${seconds}`);
+
+//3
+let num = 10;
+  num++;
+  num++;
+console.log(num);
+
+
