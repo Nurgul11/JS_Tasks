e3449f9e01af047576c4a52deec9b0cb55284810
Nurgul <abduhamidovanurgul@gmail.com>
2023-03-24 00:57:35 +0600

    start project

---
+++ 23-03/script.js
@@ -0,0 +1,54 @@
+const count = {
+    a: 5,
+    b: 6,
+    c: 10
+};
+console.log(count.a + count.b + count.c);    //  21
+
+
+//   2
+let number; 
+let date = true; 
+number = (date === true) ?  3 :  5;
+console.log(number);         //  3                   
+
+
+//    3
+
+const num = [ 10,15,9,10 ];
+const result1 = num[0] * num[1];
+const result2 = num[2] * num[3];
+const result = result1 + result2;
+console.log(result);          //  240
+
+
+//  4
+
+const arr = ['a', 'b', 'c', 'd'];
+console.log(` '${arr[0]} + ${arr[1]}, ${arr[2]} + ${arr[3]}' `);   // 'a+b, c+d'
+
+
+//  5
+//  длинной
+let test = true;
+if(!test != true){
+    alert('Верно');
+} else{
+    alert('Неверно');
+};               //  Верно
+
+//  короткий
+let test1;
+test1 = (test1 != true) ? 'Верно' : 'Неверно';
+console.log(test1);         // Верно
+
+
+
+
+                          
+
+
+
+
+
+
