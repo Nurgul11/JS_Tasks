0dcb3d94556e17bd85c09fb3609f0582b2c1df19
Nurgul <abduhamidovanurgul@gmail.com>
2023-03-28 09:17:06 +0600

    start project

--- 26-03/script.js
+++ 26-03/script.js
@@ -25,13 +25,13 @@ getName();         //  Привет Василий
 
 function calculate_exam(i, j){
     for(let i = 0; i < 100; i++){
-        if(i > 90 && j > 10){
+        if(i > 90 || j > 10){
            return 100; 
-        } else if(i >75 && j > 5){
+        } else if(i >75 || j > 5){
             return 90;
-        } else if(i >50 && j > 2){
+        } else if(i >50 || j > 2){
             return 75;
-        } else if(i >30 && j > 1){
+        } else if(i >30 || j > 1){
             return 50;
         } else{
             return 0;
@@ -39,8 +39,8 @@ function calculate_exam(i, j){
     }
     
 }
-console.log(100);
-console.log(90);
-console.log(75);
-console.log(50);
-console.log(0);
+console.log(calculate_exam(100,11));
+console.log(calculate_exam(90,7));
+console.log(calculate_exam(75,4));
+console.log(calculate_exam(40,3));
+// console.log(0);
